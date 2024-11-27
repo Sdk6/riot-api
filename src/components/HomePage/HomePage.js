289ffca3afde9b98aab157b69a6bc8de3b923b89
sdk6 <sdk6@njit.edu>
2024-11-27 00:17:06 -0500

    added banner to homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,12 +2,15 @@ import {
     Flex,
     Text
  } from "@chakra-ui/react";
+import Banner from "../Banner";
 
 const HomePage = () => {
     return(
+        <>
+        <Banner minW="100%"/>
         <Flex bg="#000e1">
-        
         </Flex>
+        </>
     )
 }
 export default HomePage;
